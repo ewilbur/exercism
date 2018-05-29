a6dcc18ea43d2f212ae25b299bf9935048adb6f6
Evan Wilbur <ewilbur9@gmail.com>
2018-05-29 17:42:31 -0500

    Removed testing files

--- haskell/sieve/src/Sieve.hs
+++ haskell/sieve/src/Sieve.hs
@@ -1,10 +1,7 @@
 -- module Sieve (primesUpTo) where
-module Main where
+module Sieve where
 
 import Control.Arrow
-import Debug.Trace
-import System.Environment
-import Control.DeepSeq
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo n = fmap fst . filter snd . sieve (ceiling . sqrt . fromIntegral $ n) $ zip [2..n] (repeat True)
@@ -15,8 +12,3 @@ sieve n (x:xs) = x : sieve (n - 1) (gaps x xs)
   where
     gaps (y,True) = zipWith second (fmap (&&) . cycle . reverse $ False : replicate (fromIntegral y - 1) True)
     gaps _ = id
-
-main = do
-  args <- getArgs
-  -- let bnd = head . read $ args
-  return ()
